61cbe775c8cce91802fec46b7d68ec0d2f6a0bef
Matthew Jackson <mattshiloh@gmail.com>
2018-10-27 14:02:29 -0500

    formatting and comments

--- javascripts/jokes.js
+++ javascripts/jokes.js
@@ -1,7 +1,9 @@
+// Begin API Call from ICanHasDadJoke.com
 const badJoke = () => {
     return new Promise((resolve, reject) => {
         $.ajax({
             url: 'https://icanhazdadjoke.com/',
+            // Headers Neccessary To Pass API
             headers: {
                 accept: 'application/json'
             }
@@ -14,17 +16,21 @@ const badJoke = () => {
             });
     });
 };
+// End API Call from ICanHasDadJoke.com
 
+// Begin Function to Call API and PTD
 const tellJoke = () => {
     badJoke()
         .then(joke => {
-            $('#joke').append(`<h2 style="color: yellow;">${joke.joke}</h2>`);
+            $('#joke').append(`<h2 style="color: violet;">${joke.joke}</h2>`);
         })
         .catch(error => {
             console.error(error);
         });
 };
+// End Function to Call API and PTD
 
+// Export Function
 export {
     tellJoke
 };
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,11 @@
+// Import Functions
 import { tellJoke } from "./jokes.js"
 
+// Function to Call Functions From All JS
 const loadApp = () => {
     tellJoke();
 }
+// End All Call Function
 
+// Call All Call Function
 loadApp();
