e6e57d25aab1f6fdad94de4de79ecd7de5036cff
Matthew Jackson <mattshiloh@gmail.com>
2018-10-27 12:18:48 -0500

    printing to DOM

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 const badJoke = () => {
     return new Promise((resolve, reject) => {
-    $.ajax('https://icanhazdadjoke.com/')
-        .done((data) => {
-            resolve(data);
-            console.log(data);
+      $.ajax({
+        url: 'https://icanhazdadjoke.com/',
+        headers: {
+          accept: 'application/json'
+        }
+      })
+        .done(randomJoke => {
+          resolve(randomJoke);
         })
         .fail(error => {
-            reject(error);
-            console.log(error);
-        })
-    })
-}
-
-badJoke();
+          reject(error);
+        });
+    });
+  };
+  
+  const tellJoke = () => {
+    badJoke()
+      .then(joke => {
+        $('#joke').append(`<h2 style="color: purple;">${joke.joke}</h2>`);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+  
+  tellJoke();
